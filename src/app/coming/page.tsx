8b8cbee0ffd9b4c96b8986806f9212267e45c5fa
Shashi Shekhar <shashishekhar@Shashis-MacBook-Air.local>
2023-09-17 22:07:57 +0530

    fixing spelling and margin top on coming soon page

--- src/app/coming/page.tsx
+++ src/app/coming/page.tsx
@@ -4,7 +4,7 @@ export default function ComingSoon() {
   return (
       <div className="min-[2000px]:max-w-screen-2xl min-[2000px]:m-auto flex flex-col content-center justify-center text-center">
         
-        <Image src="/cutedoggo.jpg" className="m-auto mt-40" alt="coming soon" width={400} height={400} />
+        <Image src="/cutedoggo.jpg" className="m-auto mt-24" alt="coming soon" width={400} height={400} />
         <p className="text-5xl">Coming soon!</p>
       </div>
   );
